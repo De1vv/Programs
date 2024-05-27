2b1ffdeb5ab3daeaae2b39fbd941dd8a421124b8
De1vv <deividperez12513@gmail.com>
2024-05-27 10:47:44 -0600

    Ejercicio para recorrer en matrices

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -4,11 +4,30 @@ personas =[]
 for i in range(3):
     cualidades = []
     nombre = input(f"Ingrese el nombre {i+1}: ")
+    #nombre="DAVID"
     cualidades.append(nombre)
     apellido_paterno = input(f"Ingrese el apellido paterno {i+1}: ")
+    #apellido_paterno = "PEREZ"
     cualidades.append(apellido_paterno)
     apellido_materno = input(f"Ingrese el apellido materno {i+1}: ")
+    apellido_materno = "QUEVEDO"
     cualidades.append(apellido_materno)
+    edad = input(f"Ingrese la edad {i+1}: ")
+    #edad = 20
+    cualidades.append(edad)
+
 
     personas.append(cualidades)
     print(personas)
+
+if len(personas) == 3:
+    print("Los datos ingresados son:")
+    for y in range(len(personas)):
+
+      for x in range(len(cualidades)):
+
+        print(f"Persona {personas[y][x]}:")
+       
+
+else:
+    print("No se han ingresado los datos de las tres personas requeridas.")
